0e33cfa6f731c72e4ef0ded110c54236a4e4018d
bluepost59 <kaput.kat@gmail.com>
2022-02-05 18:39:03 +0900

    Squashed commit of the following:
    commit 3ced841e73d2014a9648e1b16ce8fde4ac25c044
    Author: bluepost59 <kaput.kat@gmail.com>
    Date:   Sat Feb 5 17:55:02 2022 +0900
    
        test page

---
+++ app.py
@@ -0,0 +1,11 @@
+import sqlite3
+import flask as f
+
+app = f.Flask(__name__)
+
+@app.route("/")
+def app_route():
+    return "<h1>This is test</h1>"
+
+if __name__=="__main__":
+    app.run(host = "0.0.0.0",port=3000)
